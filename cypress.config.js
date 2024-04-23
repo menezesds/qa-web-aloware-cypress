54a562425a3d8df01469fd55fd336add78420291
menezesds <menezesds@hotmail.com>
2024-04-23 15:22:15 -0300

    Install and cnfigure Cypress and add gitignore file.

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  responseTimeout: 10000,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    chromeWebSecurity: false,
+    video: true,
+    baseUrl: 'https://aloware.com',
+  },
+});
